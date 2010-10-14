924b00698756e119f6f1ee1a5775dab4e16b2308
Michael Sokol <mikaa123@gmail.com>
2010-10-14 09:03:38 -0400

    Adding unit testing

--- tests/SparqlTransmissionTest.rb
+++ tests/SparqlTransmissionTest.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'net/http'
 require '../SparqlTransmission'
 
 class SparqlTransmission_Test < Test::Unit::TestCase
@@ -15,6 +16,17 @@ class SparqlTransmission_Test < Test::Unit::TestCase
         "SparqlTransmission.new(['hey'])",
         "SparqlTransmission.new({:t => :test})"
     ].each { |t| assert_raise(ArgumentError, "Error for #{t}") { eval t } }
+  end
+  
+  def test_parse_request
+    sq = SparqlTransmission.new("http://dbpedia.org/sparql")
+    res = String.new
+    File.open('sparql_response.xml').each { |line| res += line }
+    result = sq.send(:parse_request, res)
+    assert_equal(105, sq.number_of_result, "Wrong number of result")
+    assert_equal("subject", result[:variables][0], "Wrong requested variable")
+    assert_equal("http://www.openlinksw.com/schemas/virtrdf#DefaultQuadMap-G", result[:results][0][:subject], "Wrong result")
+    assert_raise(ArgumentError, "ArgumentError not raised") { sq.send(:parse_request, 1) }
   end
   
   def test_fail
